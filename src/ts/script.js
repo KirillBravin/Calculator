663fcb722ae9ed92e7159c3f438a20785f6a62f2
Phil <kshaadoo1991@gmail.com>
2024-01-25 15:45:40 +0200

    Added logic to numbers, also dynamic font-size scaling if numbers go over border and error message if too many numbers input

--- src/ts/script.js
+++ src/ts/script.js
@@ -21,28 +21,124 @@ var switch_button = document.querySelector(".switch");
 var zero = document.querySelector(".zero");
 var pointer = document.querySelector(".pointer");
 var result_button = document.querySelector(".result-button");
+result_2.classList.add("display-hidden"); // Hides second result when you start the app
 var sum = 0;
 var sum2 = 0;
-one.addEventListener("click", function () {
-    var one = 1;
-    if (result_1.textContent === "0") {
-        result_1.textContent = "1";
-        sum += one;
+var ifNumber;
+function numbers(sum, number) {
+    if (sum === 0) {
+        result_1.textContent = number.toString();
+        sum = number;
+        console.log(sum);
+        return sum;
     }
-    else if (result_1.textContent !== null) {
-        result_1.textContent = result_1.textContent.concat(one.toString());
-        sum += one;
+    else if (sum > -999999 && sum < 999999 && result_1.textContent !== null) {
+        result_1.textContent = result_1.textContent.concat(number.toString());
+        sum = +result_1.textContent;
+        console.log(sum);
+        return sum;
     }
-});
-two.addEventListener("click", function () {
-    var _a;
-    var two = 2;
-    if (result_1.textContent === "0") {
-        result_1.textContent = "2";
-        sum += two;
+    else if (sum > -9999999 && sum < 9999999 && result_1.textContent !== null) {
+        result_1.style.fontSize = "70px";
+        result_1.textContent = result_1.textContent.concat(number.toString());
+        sum = +result_1.textContent;
+        console.log(sum);
+        return sum;
+    }
+    else if (sum > -99999999 &&
+        sum < 99999999 &&
+        result_1.textContent !== null) {
+        result_1.style.fontSize = "60px";
+        result_1.textContent = result_1.textContent.concat(number.toString());
+        sum = +result_1.textContent;
+        console.log(sum);
+        return sum;
+    }
+    else if (sum > -999999999 &&
+        sum < 999999999 &&
+        result_1.textContent !== null) {
+        result_1.style.fontSize = "55px";
+        result_1.textContent = result_1.textContent.concat(number.toString());
+        sum = +result_1.textContent;
+        console.log(sum);
+        return sum;
+    }
+    else if (sum <= -999999 || sum >= 999999) {
+        result_2.classList.remove("display-hidden");
+        result_2.style.fontSize = "30px";
+        return (result_2.textContent = "Error: The number is too big");
     }
     else {
-        (_a = result_1.textContent) === null || _a === void 0 ? void 0 : _a.concat("asd");
-        sum += two;
+        return "Error";
+    }
+}
+one.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 1);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+two.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 2);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+three.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 3);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+four.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 4);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+five.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 5);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+six.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 6);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+seven.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 7);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+eight.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 8);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+nine.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 9);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
+    }
+});
+zero.addEventListener("click", function (e) {
+    e.preventDefault();
+    ifNumber = numbers(sum, 0);
+    if (typeof ifNumber === "number") {
+        sum = ifNumber;
     }
 });
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -32,27 +32,134 @@ const result_button: HTMLElement = document.querySelector(
   ".result-button"
 ) as HTMLElement;
 
+result_2.classList.add("display-hidden"); // Hides second result when you start the app
+
 let sum: number = 0;
 let sum2: number = 0;
+let ifNumber: number | string;
 
-one.addEventListener("click", () => {
-  const one = 1;
-  if (result_1.textContent === "0") {
-    result_1.textContent = "1";
-    sum += one;
-  } else if (result_1.textContent !== null) {
-    result_1.textContent = result_1.textContent.concat(one.toString());
-    sum += one;
+function numbers(sum: number, number: number): number | string {
+  if (sum === 0) {
+    result_1.textContent = number.toString();
+    sum = number;
+    console.log(sum);
+    return sum;
+  } else if (sum > -999999 && sum < 999999 && result_1.textContent !== null) {
+    result_1.textContent = result_1.textContent.concat(number.toString());
+    sum = +result_1.textContent;
+    console.log(sum);
+    return sum;
+  } else if (sum > -9999999 && sum < 9999999 && result_1.textContent !== null) {
+    result_1.style.fontSize = "70px";
+    result_1.textContent = result_1.textContent.concat(number.toString());
+    sum = +result_1.textContent;
+    console.log(sum);
+    return sum;
+  } else if (
+    sum > -99999999 &&
+    sum < 99999999 &&
+    result_1.textContent !== null
+  ) {
+    result_1.style.fontSize = "60px";
+    result_1.textContent = result_1.textContent.concat(number.toString());
+    sum = +result_1.textContent;
+    console.log(sum);
+    return sum;
+  } else if (
+    sum > -999999999 &&
+    sum < 999999999 &&
+    result_1.textContent !== null
+  ) {
+    result_1.style.fontSize = "55px";
+    result_1.textContent = result_1.textContent.concat(number.toString());
+    sum = +result_1.textContent;
+    console.log(sum);
+    return sum;
+  } else if (sum <= -999999 || sum >= 999999) {
+    result_2.classList.remove("display-hidden");
+    result_2.style.fontSize = "30px";
+    return (result_2.textContent = "Error: The number is too big");
+  } else {
+    return "Error";
+  }
+}
+
+one.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 1);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
   }
 });
 
-two.addEventListener("click", () => {
-  const two = 2;
-  if (result_1.textContent === "0") {
-    result_1.textContent = "2";
-    sum += two;
-  } else {
-    result_1.textContent?.concat("asd");
-    sum += two;
+two.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 2);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+three.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 3);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+four.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 4);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+five.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 5);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+six.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 6);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+seven.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 7);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+eight.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 8);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+nine.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 9);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
+  }
+});
+
+zero.addEventListener("click", (e) => {
+  e.preventDefault();
+  ifNumber = numbers(sum, 0);
+  if (typeof ifNumber === "number") {
+    sum = ifNumber;
   }
 });
